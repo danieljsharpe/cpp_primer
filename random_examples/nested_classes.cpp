eb8e204c35c4902a2a58aab7e3b30017a573bb64
danieljsharpe <daniel.j.sharpe1995@gmail.com>
2019-01-10 19:01:07 +0000

    finished nested_classes

--- random_examples/nested_classes.cpp
+++ random_examples/nested_classes.cpp
@@ -10,30 +10,44 @@ the usual access rules shall be obeyed.
 using namespace std; 
   
 class Enclosing {       
-    private:
 
-    int x = 1; 
+    int x = 1;
+    class Nested2 {};
+
+    public:
      
     class Nested { 
+
         int y = 2;
-        void NestedFun(Enclosing *e) {
-        cout << e->x;  // works fine: nested class can access  
-                       // private members of Enclosing class
-        }
+        Nested2* nested2; // allowed because Nested can access private members of enclosing (such as Nested2)
+
         public:
-        int z = 3;
+
+        void nested_func(Enclosing *e) {
+            Nested2 *nested2inst = new Nested2;
+            delete nested2inst;
+            cout << e->x << endl;  // works fine: nested class can access  
+                                   // private members of Enclosing class
+        }
+        static int z;
     };
+
     void enclosing_func(Nested *n) {
-        // cout << n->y; // compiler error: y is private in Nested
-        cout << n->z; // okay: x is public in nested
+        // cout << n->y << endl; // compiler error: y is private in Nested
+        cout << n->z << endl; // okay: x is public in nested
     }
 };
+
+typedef Enclosing::Nested Encnest;
+int Encnest::z = 3;
   
 int main() {
 
-    Enclosing* enclosing1;
+    Enclosing *enclosing1, *enclosing2;
+    Encnest* encnest1;
 
+    encnest1->nested_func(enclosing2);
+    enclosing1->enclosing_func(encnest1);
 
     return 0;
-} 
-
+}
